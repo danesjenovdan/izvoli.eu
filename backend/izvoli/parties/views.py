b25baa65ba15fb9c1023d3fe5f3d763529684552
Patricija Brečko <patricija@danesjenovdan.si>
2024-04-28 19:12:40 +0200

    add category to backend

--- backend/izvoli/parties/views.py
+++ backend/izvoli/parties/views.py
@@ -327,6 +327,7 @@ class Volitvomat(APIView):
             statements_dict[statement.id] = {
                 "title": statement.title,
                 "description": statement.description,
+                "category": statement.statement_group.name if statement.statement_group else "",
                 "parties": {},
                 # "category": question.workgroup.id if question.workgroup else None,
             }
