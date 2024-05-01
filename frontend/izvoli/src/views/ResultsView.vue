c7605bc017c66b2d53f2d22e7f4dea328c27b1ab
LepkoQQ <lepko.san@gmail.com>
2024-05-01 11:58:57 +0200

    results view improvements

--- frontend/izvoli/src/views/ResultsView.vue
+++ frontend/izvoli/src/views/ResultsView.vue
@@ -114,7 +114,7 @@ function partyImageUrl(url) {
                 stroke="#7FB2FF"
                 stroke-width="6"
                 :stroke-dashoffset="dashOffsetFromPercentage(0)"
-                :stroke-dasharray="dashArrayFromPercentage(25)"
+                :stroke-dasharray="dashArrayFromPercentage(result.percentage)"
               />
               <circle
                 cx="43"
@@ -123,8 +123,8 @@ function partyImageUrl(url) {
                 fill="none"
                 stroke="#FFF"
                 stroke-width="6"
-                :stroke-dashoffset="dashOffsetFromPercentage(25 + 0.5)"
-                :stroke-dasharray="dashArrayFromRemainderPercentage(25 + 1)"
+                :stroke-dashoffset="dashOffsetFromPercentage(result.percentage + 0.5)"
+                :stroke-dasharray="dashArrayFromRemainderPercentage(result.percentage + 1)"
               />
               <circle cx="43" cy="43" r="35" :fill="`url(#donut-image-${result.party_id})`" />
             </svg>
@@ -144,41 +144,43 @@ function partyImageUrl(url) {
           <span>Izberi stranke za primerjavo</span>
           <button @click="compareWithAllParties">Izberi vse stranke</button>
         </p>
-        <div v-for="party in results" :key="party.party_id" class="party">
-          <label :for="`chosen-party-${party.party_id}`">
-            <input
-              type="checkbox"
-              :id="`chosen-party-${party.party_id}`"
-              :value="party.party_id"
-              v-model="chosenParties"
-            />
-            <img :src="parties[party.party_id].image" class="party-image" />
-            {{ parties[party.party_id].name }}
-          </label>
-          <div class="progress">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              :aria-valuenow="party.percentage"
-              aria-valuemin="0"
-              :aria-valuemax="100"
-              :style="{ width: `${party.percentage}%` }"
-              :class="{ 'border-end': party.percentage < 100 }"
-            ></div>
+        <div class="parties">
+          <div v-for="party in results" :key="party.party_id" class="party">
+            <label :for="`chosen-party-${party.party_id}`">
+              <input
+                type="checkbox"
+                :id="`chosen-party-${party.party_id}`"
+                :value="party.party_id"
+                v-model="chosenParties"
+              />
+              <img :src="partyImageUrl(parties[party.party_id].image)" class="party-image" />
+              {{ parties[party.party_id].name }}
+            </label>
+            <div class="progress">
+              <div
+                class="progress-bar"
+                role="progressbar"
+                :aria-valuenow="party.percentage"
+                aria-valuemin="0"
+                :aria-valuemax="100"
+                :style="{ width: `${party.percentage}%` }"
+                :class="{ 'border-end': party.percentage < 100 }"
+              ></div>
+            </div>
+            <span class="party-percentage">{{ party.percentage }} %</span>
           </div>
-          <span>{{ party.percentage }} %</span>
-        </div>
-        <div v-for="party in partiesNoAnswer" :key="party.id" class="party">
-          <label :for="`chosen-party-${party.id}`">
-            <input
-              type="checkbox"
-              :id="`chosen-party-${party.id}`"
-              :value="party.id"
-              v-model="chosenParties"
-            />
-            <img :src="party.image" class="party-image" />
-            {{ party.name }}
-          </label>
+          <div v-for="party in partiesNoAnswer" :key="party.id" class="party">
+            <label :for="`chosen-party-${party.id}`">
+              <input
+                type="checkbox"
+                :id="`chosen-party-${party.id}`"
+                :value="party.id"
+                v-model="chosenParties"
+              />
+              <img :src="partyImageUrl(party.image)" class="party-image" />
+              {{ party.name }}
+            </label>
+          </div>
         </div>
         <div class="button-wrapper">
           <button class="button-go" @click="compareWithChosenParties">
@@ -285,84 +287,94 @@ function partyImageUrl(url) {
       }
     }
 
-    .party {
-      //     display: flex;
-      //     align-items: center;
-      //     margin: 15px 0;
+    .parties {
+      margin-top: 28px;
 
-      label {
-        //       display: flex;
-        //       align-items: center;
-        //       cursor: pointer;
-        //       width: 25%;
+      .party {
+        display: flex;
+        align-items: center;
+        margin-bottom: 16px;
 
-        input[type='checkbox'] {
-          flex-shrink: 0;
-          appearance: none;
-          background-color: transparent;
-          margin: 2px 0 0 0;
-          width: 16px;
-          height: 16px;
-          border: 1px solid black;
-          border-radius: 4px;
-          transform: translateY(-0.075em);
+        label {
+          flex: 1.25;
+          display: flex;
+          align-items: center;
           cursor: pointer;
-          position: relative;
+          font-size: 15px;
+          line-height: 18px;
+          font-weight: 800;
 
-          &:checked {
-            background-color: #ffd100;
+          input[type='checkbox'] {
+            flex-shrink: 0;
+            appearance: none;
+            background-color: transparent;
+            margin: 2px 0 0 0;
+            width: 16px;
+            height: 16px;
+            border: 1px solid black;
+            border-radius: 4px;
+            transform: translateY(-0.075em);
+            cursor: pointer;
+            position: relative;
 
-            &::before {
-              content: '';
-              position: absolute;
-              left: 0;
-              top: 0;
-              width: 9px;
-              height: 5px;
-              border: solid black;
-              border-width: 0 0 2px 2px;
-              transform: scale(1) rotate(-45deg) translateX(-12%) translateY(90%);
+            &:checked {
+              background-color: #ffd100;
+
+              &::before {
+                content: '';
+                position: absolute;
+                left: 0;
+                top: 0;
+                width: 9px;
+                height: 5px;
+                border: solid black;
+                border-width: 0 0 2px 2px;
+                transform: scale(1) rotate(-45deg) translateX(-12%) translateY(90%);
+              }
+            }
+          }
+
+          .party-image {
+            flex-shrink: 0;
+            margin-left: 16px;
+            margin-right: 9px;
+            width: 36px;
+            height: 36px;
+            background-color: #ffffff;
+            border: 1px solid black;
+            border-radius: 9999px;
+            object-fit: contain;
+          }
+        }
+
+        .progress {
+          flex: 2;
+          height: 20px;
+          margin-left: 10px;
+          background-color: #ffffff;
+          border: 1px solid black;
+          border-radius: 10px;
+          overflow: hidden;
+
+          .progress-bar {
+            background-color: #65a3ff;
+            height: 100%;
+            border-radius: inherit;
+
+            &.border-end {
+              border-right: 1px solid black;
             }
           }
         }
 
-        //       .party-image {
-        //         width: 34px;
-        //         height: 34px;
-        //         border: 1px solid black;
-        //         border-radius: 18px;
-        //         margin: 0 10px;
-        //       }
+        .party-percentage {
+          flex: 0.25;
+          text-align: right;
+          font-size: 15px;
+          line-height: 18px;
+          font-weight: 500;
+        }
       }
-
-      //     .progress {
-      //       margin: 0;
-      //       width: 300px;
-      //       height: 20px;
-      //       background-color: #ffffff;
-      //       border: 1px solid black;
-      //       border-radius: 10px;
-      //       overflow: visible;
-
-      //       .progress-bar {
-      //         background-color: #65a3ff;
-      //         border-radius: 10px;
-      //         height: 100%;
-      //         position: relative;
-      //         overflow: visible;
-      //         transition: width 0.5s;
-
-      //         &.border-end {
-      //           border-right: 1px solid black;
-      //         }
-      //       }
-      //     }
-
-      //     span {
-      //       margin-left: 10px;
-      //       font-size: 15px;
-      //       font-weight: 500;
-      //     }
     }
   }
 }
