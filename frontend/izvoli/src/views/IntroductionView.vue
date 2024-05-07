911ede57dae381c4980c820e731ba06e8772d6b9
Patricija Brečko <patricija@danesjenovdan.si>
2024-05-07 12:03:17 +0200

    update texts

--- frontend/izvoli/src/views/IntroductionView.vue
+++ frontend/izvoli/src/views/IntroductionView.vue
@@ -28,29 +28,17 @@ onMounted(() => {
   <main class="container">
     <div class="body">
       <p class="introduction">
-        Politične stranke, med katerimi bomo izbirali na volitvah 24. 4. 2022, imajo različne
-        programe in prioritete. Ker želimo volivkam in volivcem olajšati odločitev, smo razvili
-        Volitvomat, s pomočjo katerega lahko vsak preveri, s katerimi strankami se po stališčih in
-        prepričanjih najbolj ujema.
+        Želiš izvedeti, kako si prihodnost Evropske unije predstavljajo politične stranke, ki bodo nastopile na
+        prihajajočih volitvah v Evropski parlament?
       </p>
       <p>
-        Orodje temelji na odgovorih strank na 138 zahtev, ki so jih skupaj postavile civilnodružbene
-        organizacije, združene v iniciativo Glas ljudstva. H konkretnim odzivom smo večkrat pozvali
-        prav vse stranke, toda številne svojih zavez niso želele prispevati. A ni še prepozno – vse
-        stranke, ki nam bodo posredovale svoje zaveze, bomo takoj vključili v Volitvomat.
-      </p>
-      <p>
-        V Volitvomatu je zbranih 40 zahtev, na katere lahko podaš svoj odgovor. Na podlagi
-        rezultatov izveš, katera izmed sodelujočih strank najbolj ustreza tvojim stališčem, lahko pa
-        tudi podrobneje raziščeš, na katerih točkah so razhajanja največja in zakaj.
-      </p>
-      <p>
-        In za konec: ne pozabi deliti povezave do Volitvomata s prijateljicami in prijatelji, saj
-        jim lahko tako olajšaš odločitev.
+        Z aplikacijo Izvoli.eu lahko preprosto preveriš, s katerimi strankami se najbolj ujemajo tvoja politična
+        prepričanja. Odgovori na 30 trditev, primerjaj svoje odgovore s stališči strank in 9. junija sprejmi bolj
+        premišljeno odločitev.
       </p>
       <div class="button-wrapper">
         <RouterLink to="/vprasanje/1" class="button-go">
-          Vstopi <img src="../assets/img/puscica.svg" alt="" />
+          Začni <img src="../assets/img/puscica.svg" alt="" />
         </RouterLink>
       </div>
     </div>
